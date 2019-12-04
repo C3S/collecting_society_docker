4d664ec1d6140f90b6a4f8d9fd9cd87fbd54799d
Alexander Blum <alexander.blum@c3s.cc>
2019-12-04 22:08:21 +0100

    fixes .env, changes port of echoprint server

--- services/build/fingerprint.sh
+++ services/build/fingerprint.sh
@@ -2,6 +2,6 @@
 
 /bin/ttserver -port 1978 -dmn -pid /tmp/tokyotyrant.pid -log /var/log/tokyotyrant.log /opt/echoprint-data/casket.tch#bnum=100000
 /bin/sh -c "cd /opt/echoprint-server/solr/solr/ && /usr/bin/java -Dsolr.solr.home=/opt/echoprint-server/solr/solr/solr/ -Djava.awt.headless=true -jar start.jar 2>&1 > /var/log/solr.log" &
-/bin/sh -c "python /opt/echoprint-server/API/api.py 8080 2>&1 > /var/log/echorint.log" &
+/bin/sh -c "python /opt/echoprint-server/API/api.py 80 2>&1 > /var/log/echorint.log" &
 
 tail -f /var/log/tokyotyrant.log /var/log/solr.log /var/log/echoprint.log
