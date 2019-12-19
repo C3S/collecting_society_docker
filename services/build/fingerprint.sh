a2378a7e26117bbde25a294fac6bc993dbe5f3e4
Thomas Mielke <mielket@gmx.de>
2019-12-19 18:47:44 +0100

    tried to debug the fingerprinter with ptvsd

--- services/build/fingerprint.sh
+++ services/build/fingerprint.sh
@@ -3,11 +3,11 @@
 INGEST=$(ls /opt/echoprint-data | grep casket)
 
 /bin/ttserver -port 1978 -dmn -pid /tmp/tokyotyrant.pid -log /var/log/tokyotyrant.log /opt/echoprint-data/casket.tch#bnum=100000
-/bin/sh -c "cd /opt/echoprint-server/solr/solr/ && /usr/bin/java -Dsolr.solr.home=/opt/echoprint-server/solr/solr/solr/ -Djava.awt.headless=true -jar start.jar 2>&1 > /var/log/solr.log" &
-/bin/sh -c "python /opt/echoprint-server/API/api.py 80 2>&1 > /var/log/echorint.log" &
+/bin/sh -c "cd /shared/src/echoprint-server/solr/solr/ && /usr/bin/java -Dsolr.solr.home=/shared/src/echoprint-server/solr/solr/solr/ -Djava.awt.headless=true -jar start.jar 2>&1 > /var/log/solr.log" &
+/bin/sh -c "python /shared/src/echoprint-server/API/api.py 80 2>&1 > /var/log/echorint.log" &
 
 if [ $INGEST ]; then
-    cd /opt/echoprint-server/util && /usr/bin/python /opt/echoprint-server/util/fastingest.py /root/fingerprint-data.json
+    cd /shared/src/echoprint-server/util && python fastingest.py /root/fingerprint-data.json
 fi
 
 tail -f /var/log/tokyotyrant.log /var/log/solr.log /var/log/echoprint.log
