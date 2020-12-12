1a32e8f636fa5dc2cbc22b1c0d9b5b8afabe112b
Thomas Mielke <mielket@gmx.de>
2020-12-12 18:23:46 +0100

    added split and sampler releases to new data importer

--- volumes/shared/data/datasets/release.py
+++ volumes/shared/data/datasets/release.py
@@ -17,9 +17,10 @@ DEPENDS = [
     'artist',
     'publisher',
     'label',
-    'genres_and_styles'
+    'genres_and_styles',
+    'license',
+    'web_user'
 ]
-
 
 def generate(reclimit):
 
@@ -29,15 +30,24 @@ def generate(reclimit):
     genres_per_release = reclimit or 2
     styles_per_release = reclimit or 2
     release_cancellation_chance = reclimit and 1 or 0.3
+    sampler_releases = reclimit or 1
+    creations_per_sampler = reclimit or 5
+    split_releases = reclimit or 1
+    artists_per_split_release = reclimit or 2
+    creations_per_split_artist = reclimit or 2
 
     # models
     Artist = Model.get('artist')
+    Creation = Model.get('creation')
     Release = Model.get('release')
+    ReleaseTrack = Model.get('release.track')
     Publisher = Model.get('publisher')
     Genre = Model.get('genre')
     Style = Model.get('style')
     Label = Model.get('label')
     Country = Model.get('country.country')
+    WebUser = Model.get('web.user')
+    License = Model.get('license')
 
     artists = Artist.find([('claim_state', '!=', 'unclaimed')])
     countries = Country.find([])
@@ -90,3 +100,126 @@ def generate(reclimit):
             owner_artist, = Artist.find([('id', '=', owner.id)])
             release.artists.append(owner_artist)
             release.save()
+
+        # Sampler Releases
+    for i in range(1, sampler_releases + 1):
+        number = i
+        solos = Artist.find([
+            ('claim_state', '!=', 'unclaimed'),
+            ('group', '=', False)])
+        labels = Label.find([])
+        genres = Genre.find([])
+        styles = Style.find([])
+        isrc = ''.join(random.sample(string.ascii_uppercase, 3)) + \
+            str(random.randint(1,999999999)).zfill(9)
+        creations = Creation.find([('claim_state', '!=', 'unclaimed')])
+        publishers = Publisher.find([])
+        creator = random.choice(solos)
+        web_user, = WebUser.find([('party.id', '=', creator.party.id)])
+
+        # release
+        release = Release(
+            type="compilation",
+            entity_creator=creator.party,
+            commit_state='commited',
+            claim_state='claimed',
+            title="Sampler %s" % str(number).zfill(3),
+            genres=random.sample(genres, min(
+                genres_per_release, len(genres))),
+            styles=random.sample(styles, min(
+                styles_per_release, len(styles))),
+            warning='WARNING: This is testdata!',
+            distribution_territory=random.choice(countries).code,
+            label=random.choice(labels),
+            label_catalog_number=str(random.randint(10000, 99999)),
+            publisher=random.choice(publishers)
+        )
+        release.save()
+
+        # release creation
+        tracks = random.sample(creations, creations_per_sampler)
+        last_date = datetime.date(1,1,1)
+        for i in range(0, len(tracks)):
+            track = tracks[i]
+            if track.release.release_date > last_date:
+                last_date = track.release.release_date
+            licenses = License.find([])
+            rc = ReleaseTrack()
+            rc.creation=track
+            rc.release=release
+            rc.title="Renamed Song %s on a Compilation" % str(
+                number).zfill(3)
+            rc.medium_number=1
+            rc.track_number=i
+            rc.license=random.choice(licenses)
+            rc.save()
+
+        release.production_date = last_date + datetime.timedelta(50)
+        release.copyright_date = last_date + datetime.timedelta(80)
+        release.release_date = last_date + datetime.timedelta(100)
+        release.online_release_date = last_date + datetime.timedelta(100)
+        release.save()
+
+    # Split Releases
+    for i in range(1, split_releases + 1):
+        number = i
+        artists = Artist.find([('claim_state', '!=', 'unclaimed')])
+        labels = Label.find([])
+        genres = Genre.find([])
+        styles = Style.find([])
+        isrc = ''.join(random.sample(string.ascii_uppercase, 3)) + \
+            str(random.randint(1,999999999)).zfill(9)
+        creations = Creation.find([('claim_state', '!=', 'unclaimed')])
+        publishers = Publisher.find([])
+
+        splits = random.sample(artists, artists_per_split_release)
+        creator = splits[0]
+        if creator.group:
+            creator = creator.solo_artists[0]
+
+        # release
+        release = Release(
+            type='split',
+            entity_creator=creator.party,
+            commit_state='commited',
+            claim_state='claimed',
+            title="Split Release %s" % str(number).zfill(3),
+            genres=random.sample(genres, min(
+                genres_per_release, len(genres))),
+            styles=random.sample(styles, min(
+                styles_per_release, len(styles))),
+            warning='WARNING: This is testdata!',
+            distribution_territory='Germany',
+            label=random.choice(labels),
+            label_catalog_number='12345',
+            publisher=random.choice(publishers)
+        )
+        for split in splits:
+            release.artists.append(split)
+        release.save()
+
+        # release creation
+        last_date = datetime.date(1,1,1)
+        for split in splits:
+            tracks = random.sample(split.creations,
+                min(creations_per_split_artist,len(split.creations)))
+            for i in range(0, len(tracks)):
+                track = tracks[i]
+                if track.release.release_date > last_date:
+                    last_date = track.release.release_date
+                licenses = License.find([])
+                rc = ReleaseTrack()
+                rc.creation=track
+                rc.release=release
+                rc.title="Renamed Song %s on a Split Release" % str(
+                    number).zfill(3)
+                rc.medium_number=1
+                rc.track_number=i
+                rc.license=random.choice(licenses)
+                rc.save()
+
+        release.production_date = last_date + datetime.timedelta(50)
+        release.copyright_date = last_date + datetime.timedelta(80)
+        release.release_date = last_date + datetime.timedelta(100)
+        release.online_release_date = last_date + datetime.timedelta(100)
+        release.save()
