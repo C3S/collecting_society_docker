0f236226567eb7ec30ae16698dd14d5261060b94
Alexander Blum <alexander.blum@c3s.cc>
2021-03-27 07:42:28 +0100

    ref #1019: adds --help flags to the scripts

--- volumes/shared/docs/build.sh
+++ volumes/shared/docs/build.sh
@@ -2,40 +2,50 @@
 # For copyright and license terms, see COPYRIGHT.rst (top level of repository)
 # Repository: https://github.com/C3S/collecting_society_docker
 #
-# Usage: ./docs-build [--no-autoapi]
-#
-# Options:
-#     --no-autoapi: don't parse the modules
+#% Usage: ./docs-build [--no-autoapi] [--help]
+#%
+#%   This script builds the documentation with sphinx.
+#%
+#% Options:
+#%   --no-autoapi: don't parse the modules
+#%   --help: display this help
 
+# print usage
+usage() {
+  [ "$*" ] && echo "$0: $*"
+  sed -n '/^#%/,/^$/s/^#% \{0,1\}//p' "$0"
+  exit 2
+} 2>/dev/null
+
+# get options
 NOAUTOAPI=false
 for i in "$@"; do
     if [ $i = "--no-autoapi" ]; then NOAUTOAPI=true; continue; fi
+    if [ $i = "--help" ]; then usage 2>&1; fi
 done
 
+# build docs
 if ! $NOAUTOAPI; then
     rm -rf build/doctrees
     rm -rf build/html
     rm -rf source/generated
+    mkdir -p source/collecting_society
     sphinx-apidoc -f -o source/generated --no-toc ../src/collecting_society
     sphinx-apidoc -f -o source/generated --no-toc ../src/collecting_society_web
     sphinx-apidoc -f -o source/generated --no-toc ../src/collecting_society_worker
     sphinx-apidoc -f -o source/generated --no-toc ../src/portal_web
 fi
 
-# echo ".. title:: Collecting Society Docker Setup" > source/generated/collecting_society_docker_README.rst
+# copy readmes
 cp /collecting_society_docker/README.rst source/generated/collecting_society_docker_README.rst
-
-# echo ".. title:: Collecting Society Web GUI" > source/generated/collecting_society_web_README.rst
 cp ../src/collecting_society_web/README.rst source/generated/collecting_society_web_README.rst
-echo "Files in this folder will be overwritten by the build.sh script" >> source/generated/WARNING_DO_NOT_CHANGE_FILES_HERE
-
-# echo ".. title:: Collecting Society Web GUI" > source/generated/collecting_society_web_README.rst
-mkdir source/collecting_society
-echo "Files in this folder will be overwritten by the build.sh script" >> source/collecting_society/WARNING_DO_NOT_CHANGE_FILES_HERE
 cp ../src/collecting_society/*.rst source/collecting_society
-
-# echo ".. title:: Portal Web GUI" > source/generated/portal_web_README.rst
 cp ../src/portal_web/README.rst source/generated/portal_web_README.rst
 
+# add notes
+echo "Files in this folder will be overwritten by the build.sh script" >> source/generated/WARNING_DO_NOT_CHANGE_FILES_HERE
+echo "Files in this folder will be overwritten by the build.sh script" >> source/collecting_society/WARNING_DO_NOT_CHANGE_FILES_HERE
+
+# make html
 make html
 
