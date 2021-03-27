e7ddf1d592a84c7fa9b042092804c2634a3c22ff
Alexander Blum <alexander.blum@c3s.cc>
2021-03-27 08:06:14 +0100

    ref #1019: fixes no-autoapi switch by symlinks instead of copies of rst files

--- volumes/shared/docs/build.sh
+++ volumes/shared/docs/build.sh
@@ -24,27 +24,34 @@ for i in "$@"; do
     if [ $i = "--help" ]; then usage 2>&1; fi
 done
 
-# build docs
+# generate apidoc
 if ! $NOAUTOAPI; then
+    # delete files
     rm -rf build/doctrees
     rm -rf build/html
     rm -rf source/generated
     mkdir -p source/collecting_society
+    # parse code
     sphinx-apidoc -f -o source/generated --no-toc ../src/collecting_society
     sphinx-apidoc -f -o source/generated --no-toc ../src/collecting_society_web
     sphinx-apidoc -f -o source/generated --no-toc ../src/collecting_society_worker
     sphinx-apidoc -f -o source/generated --no-toc ../src/portal_web
 fi
 
-# copy readmes
-cp /collecting_society_docker/README.rst source/generated/collecting_society_docker_README.rst
-cp ../src/collecting_society_web/README.rst source/generated/collecting_society_web_README.rst
-cp ../src/collecting_society/*.rst source/collecting_society
-cp ../src/portal_web/README.rst source/generated/portal_web_README.rst
+# link readmes
+ln -sf /collecting_society_docker/README.rst source/generated/collecting_society_docker_README.rst
+ln -sf /shared/src/collecting_society_web/README.rst source/generated/collecting_society_web_README.rst
+ln -sf /shared/src/collecting_society/README.rst source/collecting_society/README.rst
+ln -sf /shared/src/collecting_society/INSTALL.rst source/collecting_society/INSTALL.rst
+ln -sf /shared/src/collecting_society/CHANGELOG.rst source/collecting_society/CHANGELOG.rst
+ln -sf /shared/src/collecting_society/COPYRIGHT.rst source/collecting_society/COPYRIGHT.rst
+ln -sf /shared/src/portal_web/README.rst source/generated/portal_web_README.rst
 
-# add notes
-echo "Files in this folder will be overwritten by the build.sh script" >> source/generated/WARNING_DO_NOT_CHANGE_FILES_HERE
-echo "Files in this folder will be overwritten by the build.sh script" >> source/collecting_society/WARNING_DO_NOT_CHANGE_FILES_HERE
+# add warnings
+WARNING_MESSAGE="Files in this folder will be overwritten by the build.sh script"
+WARNING_FILENAME="WARNING_DO_NOT_CHANGE_FILES_HERE"
+echo $WARNING_MESSAGE >> source/generated/$WARNING_FILENAME
+echo $WARNING_MESSAGE >> source/collecting_society/$WARNING_FILENAME
 
 # make html
 make html
