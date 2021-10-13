ce3f83865bb026a5ed91db030843afd62f50f21a
Thomas Mielke <thomas.mielke@c3s.cc>
2021-10-13 02:58:24 +0200

    "erpserver threw an exception if ptvsd was enabled"

--- volumes/shared/deploy/erpserver.py
+++ volumes/shared/deploy/erpserver.py
@@ -2,13 +2,17 @@
 # EASY-INSTALL-DEV-SCRIPT: 'trytond==3.4.19','trytond'
 
 import ptvsd
+import socket
 
 __requires__ = 'trytond'
 __import__('pkg_resources').require('trytond')
 __file__ = '/shared/src/trytond/bin/trytond'
 with open(__file__) as f:
 
-    ptvsd.enable_attach(address=("0.0.0.0", 51005), redirect_output=True)
+    try:
+        ptvsd.enable_attach(address=("0.0.0.0", 51005), redirect_output=True)
+    except socket.error:
+        pass
     # ptvsd.wait_for_attach(); ptvsd.break_into_debugger()
 
     exec(compile(f.read(), __file__, 'exec'))
